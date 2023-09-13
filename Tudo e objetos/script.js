7445d1f20b5f1132d4899e99db7576bbf8208e0c
Eric Ranzani <eric_ranzani@yahoo.com.br>
2023-09-13 11:53:00 -0300

    aula Tudo é Objeto mais exercicios praticos

---
+++ Tudo e objetos/script.js
@@ -0,0 +1,17 @@
+// nomeie 3 propriedades ou métodos de strings
+var nome = "Eric";
+//nome.charCodeAt
+//nome.length
+//nome.slice
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+var btn = document.querySelector(".btn");
+//btn.addEventListener
+//btn.append
+//btn.animate
+//btn.innerHTML
+//btn.outerHTML
+
+// busque na web um objeto (método) capaz de interagir com o clipboard,
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+/* clipboard.js uma api com varios metodos e uma delas é a do clipboard */
