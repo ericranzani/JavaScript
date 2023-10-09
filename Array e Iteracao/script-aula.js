51f33e402f8056922bb9438c89fe8a1c143bf16a
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2023-10-09 13:32:19 -0300

    continuação da aula de array e iteração

--- Array e Iteracao/script-aula.js
+++ Array e Iteracao/script-aula.js
@@ -240,3 +240,53 @@ const arraysCheias = frutas2.every((fruta) => {
 
 const numeros5 = [6, 43, 22, 88, 101, 29];
 const maiorQue3 = numeros.every((x) => x > 3); // true
+
+//[].FIND() E [].FINDINDEX()
+//[].find(), retorna o valor atual da primeira iteração que retornar um valor truthy. Já o [].findIndex(), ao invés de retornar o valor, retorna o index deste valor na array.
+const frutas3 = ["Banana", "Pêra", "Uva", "Maçã"];
+const buscaUva = frutas3.findIndex((fruta) => {
+  return fruta === "Uva";
+});
+buscaUva; // 2
+
+const numeros6 = [6, 43, 22, 88, 101, 29];
+const buscaMaior45 = numeros6.find((x) => x > 45);
+buscaMaior45; // 88
+
+//[].FILTER()
+//[].filter(), retorna uma array com a lista de valores que durante a sua iteração retornaram um valor truthy.
+// no caso aq esta retornando todos os valores que são true
+const frutas4 = ["Banana", undefined, null, "", "Uva", 0, "Maçã"];
+const arrayLimpa = frutas4.filter((fruta) => {
+  return fruta;
+});
+arrayLimpa; // ['Banana', 'Uva', 'Maçã']
+
+const numeros7 = [6, 43, 22, 88, 101, 29];
+const buscaMaior50 = numeros7.filter((x) => x > 50);
+buscaMaior50; // [88, 101]
+
+//FILTER EM OBJETOS
+const aulas4 = [
+  {
+    nome: "HTML 1",
+    min: 15,
+  },
+  {
+    nome: "HTML 2",
+    min: 10,
+  },
+  {
+    nome: "CSS 1",
+    min: 20,
+  },
+  {
+    nome: "JS 1",
+    min: 25,
+  },
+];
+
+const aulasMaiores = aulas4.filter((aula) => {
+  return aula.min > 15;
+});
+aulasMaiores; // [{nome: 'CSS 1', min: 20}, {nome: 'JS 1', min: 25}]
