d00add2d3238172bcbcae5107db1eb081cc65994
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2023-11-27 13:34:43 -0300

    Exercicios práticos de fixação da aula de Object

--- Objetos/Object/script.js
+++ Objetos/Object/script.js
@@ -1,9 +1,24 @@
 // Crie uma função que verifique
 // corretamente o tipo de dado
+function verificarDado(dado) {
+  return Object.prototype.toString.call(dado);
+}
+console.log(verificarDado(3)); //Object Number
+console.log(verificarDado("3")); //Object String
+console.log(verificarDado([])); //Object Array
+console.log(verificarDado({})); //Object Object
 
 // Crie um objeto quadrado com
 // a propriedade lados e torne
 // ela imutável
+const quadrado = {};
+Object.defineProperties(quadrado, {
+  lados: {
+    value: 4,
+    enumerable: true,
+  },
+});
+console.log(quadrado);
 
 // Previna qualquer mudança
 // no objeto abaixo
@@ -12,7 +27,10 @@ const configuracao = {
   height: 600,
   background: "#333",
 };
+Object.freeze(configuracao);
 
 // Liste o nome de todas
 // as propriedades do
 // protótipo de String e Array
+console.log(Object.getOwnPropertyDescriptors(Array));
+console.log(Object.getOwnPropertyDescriptors(String));
