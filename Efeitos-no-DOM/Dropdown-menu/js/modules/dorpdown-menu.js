184f5be832b7cd2d0a9609a7e2b0f801ba763656
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-01-12 12:01:28 -0300

    criando a função de click outside para quando clicar fora da box do (sobre) ele fechar a box, utilizzando o efeito bubble

--- Efeitos-no-DOM/Dropdown-menu/js/modules/dorpdown-menu.js
+++ Efeitos-no-DOM/Dropdown-menu/js/modules/dorpdown-menu.js
@@ -1,16 +1,20 @@
-//isolando o codigo do vagedor por tabs
-export default function initDropdownMenu() {}
+import outsideClick from "./outsideclick.js";
 
-const dropdownMenus = document.querySelectorAll("[data-dropdown]");
+export default function initDropdownMenu() {
+  const dropdownMenus = document.querySelectorAll("[data-dropdown]");
 
-dropdownMenus.forEach((menu) => {
-  //dessa forma utilizamos tanto evento para desktop=click quanto o evento para mobile=touchstart
-  ["touchstart", "click"].forEach((userEvent) => {
-    menu.addEventListener(userEvent, handleClick);
+  dropdownMenus.forEach((menu) => {
+    //dessa forma utilizamos tanto evento para desktop=click quanto o evento para mobile=touchstart
+    ["touchstart", "click"].forEach((userEvent) => {
+      menu.addEventListener(userEvent, handleClick);
+    });
   });
-});
 
-function handleClick(event) {
-  event.preventDefault();
-  this.classList.toggle("active");
+  function handleClick(event) {
+    event.preventDefault();
+    this.classList.add("active");
+    outsideClick(this, ["touchstart", "click"], () => {
+      this.classList.remove("active");
+    });
+  }
 }
