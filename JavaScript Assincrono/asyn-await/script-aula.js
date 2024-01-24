166229f89abe968f8544b93780b9e2b500261e71
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-01-24 11:58:44 -0300

    iniciando aula sobre async e await

---
+++ JavaScript Assincrono/asyn-await/script-aula.js
@@ -0,0 +1,2 @@
+/*ASYNC / AWAIT
+A palavra chave async indica que a função possui partes assíncronas e que você pretende esperar a resolução da mesma antes de continuar. O await irá indicar a promise que devemos esperar. Faz parte do ES8.*/
