e90385d492280ca9fbbb8887ba1ad194037910d9
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-01-25 10:37:16 -0300

    Finalizada a aula sobre hisoty api, exemplo de como simpliicar a pagina

--- JavaScript Assincrono/history-api/script-aula.js
+++ JavaScript Assincrono/history-api/script-aula.js
@@ -1,75 +1,55 @@
-/*ASYNC / AWAIT
-A palavra chave async indica que a função possui partes assíncronas e que você pretende esperar a resolução da mesma antes de continuar. O await irá indicar a promise que devemos esperar. Faz parte do ES8.
-async function puxarDados() {
-  const responseDados = await fetch("./dados.json");
-  const jsonDados = await responseDados.json();
+/*HISTORY
+É possível acessarmos o histórico de acesso do browser em uma sessão específica através do window.history. O objeto history possui diferentes métodos e propriedades.
+pushState dificilmente é utilizado sozinho, se usa junto com o Fetch*/
 
-  document.body.innerText = jsonDados[0].aula;
+/*PUSHSTATE()
+A parte interessante de manipularmos o history é que podemos modificar o histórico e adicionar um novo item. window.history.pushState(obj, title, url).
+- Em obj podemos enviar um objeto com dados
+- mas o seu uso é restrito por isso geralmente utilizamos
+- null. O title ainda é ignorado por alguns browsers, também
+- utilizamos null nele. O url que é parte importante.*/
+//window.history.pushState(null, null, "teste.html");
 
-  //console.log(jsonDados[0].aula);
+/*POPSTATE
+O evento popstate pode ser adicionado ao objeto window. Assim podemos executar uma função toda vez que o usuário clicar no botão de voltar ou próximo
+window.history.pushState(null, null, "sobre.html");
+
+window.addEventListener("popstate", () => {
+  console.log("teste");
+});*/
+
+/*FETCH E HISTORY
+Ao puxarmos dados via fetch api, o url da página continua o mesmo. Ao combinar fetch com a history api conseguimos simular uma navegação real entre páginas, sem a necessidade de recarregamento da mesma.*/
+const links = document.querySelectorAll("a");
+
+function handleClick(event) {
+  event.preventDefault();
+  fetchPage(event.target.href);
+  window.history.pushState(null, null, event.target.href);
 }
 
-puxarDados();*/
-
-/*THEN / ASYNC
-A diferença é uma sintaxe mais limpa.
-function iniciarFetch() {
-  fetch("./dados.json")
-    .then((dadosResponse) => dadosResponse.json())
-    .then((dadosJSON) => {
-      document.body.innerText = dadosJSON.titulo;
-    });
+async function fetchPage(url) {
+  document.querySelector(".content").innerHTML = "Carregando"; //Esse codigo é pra quando a internet é lenta ou site é muito carregado, nesse caso irá aparecer a mensagem "Carregando".
+  const pageResponse = await fetch(url);
+  const pageText = await pageResponse.text();
+  replaceContent(pageText);
 }
-iniciarFetch();
 
-async function iniciarAsync() {
-  const dadosResponse = await fetch("./dados.json");
-  const dadosJSON = await dadosResponse.json();
-  document.body.innerText = dadosJSON.titulo;
+function replaceContent(newText) {
+  const newHtml = document.createElement("div");
+  newHtml.innerHTML = newText;
+
+  const oldContente = document.querySelector(".content");
+  const newContent = newHtml.querySelector(".content");
+
+  oldContente.innerHTML = newContent.innerHTML;
+  document.title = newHtml.querySelector("title").innerText;
 }
-iniciarAsync();*/
 
-/*TRY / CATCH
-Para lidarmos com erros nas promises, podemos utilizar o try e o catch na função.*/
-async function puxarDados() {
-  try {
-    const responseDados = await fetch("./dados.json");
-    const jsonDados = await responseDados.json();
-    document.body.innerText = jsonDados[0].aula;
-  } catch (erro) {
-    console.log(erro);
-  }
-}
-puxarDados();
+window.addEventListener("popstate", () => {
+  fetchPage(window.location.href);
+});
 
-/*INICIAR FETCH AO MESMO TEMPO
-Não precisamos esperar um fetch para começarmos outro. Porém precisamos esperar a resposta resolvida do fetch para transformarmos a response em json.*/
-async function iniciarAsync() {
-  const dadosResponse = fetch("./dados.json");
-  const clientesResponse = fetch("./clientes.json");
-
-  // ele espera o que está dentro da expressão () ocorrer primeiro
-  const dadosJSON = await (await dadosResponse).json();
-  const clientesJSON = await (await clientesResponse).json();
-
-  console.log(dadosJSON);
-  console.log(clientesJSON);
-}
-iniciarAsync();
-
-/*PROMISE
-O resultado da expressão à frente de await tem que ser uma promise. E o retorno do await será sempre o resultado desta promise.*/
-async function asyncSemPromise() {
-  // Console não irá esperar.
-  await setTimeout(() => console.log("Depois de 1s"), 3000);
-  console.log("acabou");
-}
-asyncSemPromise();
-
-async function iniciarAsync() {
-  await new Promise((resolve) => {
-    setTimeout(() => resolve(), 1000);
-  });
-  console.log("Depois de 1s");
-}
-iniciarAsync();
+links.forEach((link) => {
+  link.addEventListener("click", handleClick);
+});
