72e42bd8d5f12101e81259f3afd5a30ad4678173
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-01-29 11:57:08 -0300

    finalização do plugin de contagem regressiva para datas especificas

---
+++ Classe/countdown/script.js
@@ -0,0 +1,14 @@
+import Countdown from "./countdown.js";
+
+const tempoParaONatal = new Countdown("24 December 2024 23:59:59 GMT-0300");
+const tempoParaOAnoNovo = new Countdown("31 December 2024 23:59:59 GMT-0300");
+
+console.log(tempoParaONatal.total);
+setInterval(() => {
+  console.log(tempoParaOAnoNovo.total);
+}, 1000);
+
+/* console.log(tempoParaONatal.days);
+console.log(tempoParaONatal.hours);
+console.log(tempoParaONatal.minutes);
+console.log(tempoParaONatal.seconds); */
