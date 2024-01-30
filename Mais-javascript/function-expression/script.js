bcdac5b15f40b25fd46ef7f5562d96cefecdb6ce
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-01-30 11:59:53 -0300

    Feito os exercícios de fixação de function expression

--- Mais-javascript/function-expression/script.js
+++ Mais-javascript/function-expression/script.js
@@ -2,12 +2,21 @@
 //priceNumber("R$ 99,99") erro pois estava sendo chamada antes da expression ser criada
 const priceNumber = (n) => +n.replace("R$", "").replace(",", ".");
 console.log(priceNumber("R$ 99,99")); // 99.99
+
 // Crie uma IIFE e isole o escopo
 // de qualquer código JS.
+var instrumento = "Gaita";
 (() => {
-  const instrumento = "Guitarra";
+  const instrumento = "Baixo";
   console.log(instrumento);
-})();
+})(); //Baixo
+
+console.log(instrumento); //Gaita
+
 // Como podemos utilizar
 // a função abaixo.
 const active = (callback) => callback();
+
+active(function () {
+  console.log("Assim ela está sendo ativada");
+});
