9d1fb9e2adbe957d0c1f03c6d985b87688181b1a
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-01-31 10:51:10 -0300

    termino da parte 2 de destructuring e os exercícios

--- Mais-javascript/destructuring/script.js
+++ Mais-javascript/destructuring/script.js
@@ -2,9 +2,20 @@
 const btn = document.querySelector("button");
 const btnStyles = getComputedStyle(btn);
 
+const { backgroundColor, color, margin } = btnStyles;
+// console.log(btnStyles.backgroundColor);
+// console.log(btnStyles.color);
+// console.log(btnStyles.margin);
+console.log(backgroundColor, color, margin);
+
 // Troque os valores das variáveis abaixo
 let cursoAtivo = "JavaScript";
 let cursoInativo = "HTML";
+
+[cursoAtivo, cursoInativo] = [cursoInativo, cursoAtivo];
+
+console.log(cursoAtivo); //HTML
+console.log(cursoInativo); //JavaScript
 
 // Corrija o erro abaixo
 const cachorro = {
@@ -13,4 +24,7 @@ const cachorro = {
   cor: "Amarelo",
 };
 
-const { bobCor: cor } = cachorro;
+//erro de sintaxe, pois primeiro vem o nome da variavel real e depois dos : um novo nome para esse dado
+const { bobCor: cor } = cachorro; // undefined
+const { cor: bobCor } = cachorro; // amarelo
+console.log(bobCor);
