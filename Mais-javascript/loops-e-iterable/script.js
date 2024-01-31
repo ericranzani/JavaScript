51d01c4a10eee7383c019358eb8bf2acca6ca01d
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-01-31 12:29:51 -0300

    termino do modulo de mais javascript

--- Mais-javascript/loops-e-iterable/script.js
+++ Mais-javascript/loops-e-iterable/script.js
@@ -1,7 +1,15 @@
 // Crie 4 li's na página
 // Utilizando o for...of
 // adicione uma classe a cada li
+const items = document.querySelectorAll("li");
+
+for (item of items) {
+  item.classList.add("teste");
+}
 
 // Utilize o for...in para listar
 // todos as propriedades e valores
 // do objeto window
+for (const windowKey in window) {
+  console.log(windowKey + ": " + window[windowKey]);
+}
