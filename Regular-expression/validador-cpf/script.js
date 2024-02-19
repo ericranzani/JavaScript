76be185810ade34a1b4aca9e0ff86fae6386a071
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-02-19 10:30:35 -0300

    validador de cpf

--- Regular-expression/validador-cpf/script.js
+++ Regular-expression/validador-cpf/script.js
@@ -5,6 +5,6 @@ const validarCpf = new ValidarCpf(cpf).iniciar();
 
 const validadorCPF = new ValidarCpf();
 
-// console.log(validadorCPF.validar("111 111 111 222"));
+//console.log(validadorCPF.validar("111 111 111 222"));
 
-// console.log(validarCpf);
+//console.log(validarCpf);
